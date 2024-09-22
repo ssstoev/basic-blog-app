ae70c47a2d01c9b5cc8307b80b214940c0ba3edd
Stefan Stoev <stoev1606@gmail.com>
2024-09-22 11:12:48 +0200

    api of ai model works now

--- ai_model/sentiment_analysis.py
+++ ai_model/sentiment_analysis.py
@@ -2,15 +2,11 @@ import requests
 from dotenv import load_dotenv, find_dotenv
 import os
 
-# Try to find and load the .env file
-env_file = find_dotenv()
-print(f"Found .env file: {env_file}")
+print("Current Working Directory:", os.getcwd())
 
 load_dotenv(dotenv_path="config.env")
 
 API_TOKEN = os.getenv("API_TOKEN")
-#API_TOKEN = "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"
-print(API_TOKEN)
 
 API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
